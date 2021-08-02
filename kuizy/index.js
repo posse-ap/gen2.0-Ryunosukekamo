31da5c5f338181a8e27ade78e1769eca7cc0c704
加茂竜之介 <Kamoryu2001@gmail.com>
2021-08-02 09:16:13 +0900

    正解の「たかなわ」を押した際に、正解は「たかなわ」と表示しかし、下の青い下線をまだ修正できていない。

--- kuizy/index.js
+++ kuizy/index.js
@@ -1,20 +1,25 @@
 "use strict";
  
-let result1 = document.getElementById('result1')
+let result1= document.getElementById("result1")
+let button1 = document.getElementById("button1");
 
-document.getElementById("button1").onclick = function() {
+button1.onclick = function() {
     this.classList.toggle("blue");
-    document.getElementById("text").style.color = "white";
-    result1.insertAdjacentHTML('afterend','<li id="result2">正解!</li>');
-    
-   
+    button1.classList.add('white');  
+    result1.insertAdjacentHTML('afterend','<li id="result2">正解! <p class= display>正解は「たかなわ」です！</p></li>');
+    let result2 = document.getElementById("result2");
+    result2.classList.add('underline'); 
+    result2.classList.add('appearance');
+    let display = document.getElementsByClassName("display")
+    display.classList.add('appearance1');
+
+
+
   }
     
 
 
   
-
-
 
 
 
