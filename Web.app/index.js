b01c98ae8c09ed7b1a160462b08fca41364801c7
加茂竜之介 <Kamoryu2001@gmail.com>
2021-11-02 06:47:08 +0900

    モーダルのクリック処理一部完成

--- Web.app/index.js
+++ Web.app/index.js
@@ -6,10 +6,10 @@ let modal_record_post = document.getElementById('modal_record_post');
 let done = document.getElementById('done');
 // let only_freamework = document.getElementById('only_freamework');
 let loader_wrap = document.getElementById('loader_wrap');
-let modal_overlay = document.getElementById('modal_overlay'); 
-let clear_btn = document.getElementById('clear_btn');
+let modal_overlay = document.getElementById('modal_overlay');
+let btn_close = document.getElementById('btn_close');
 // let clear = document.getElementsByClassName('clear_btn'); cursor pointerをjsにつけようと思ったが、つけれんかった。既にcssで書いているものは、jsで変更できない感じ？
-let input_day_for_study= document.getElementById('input_day_for_study');//学習日欄
+let input_day_for_study = document.getElementById('input_day_for_study');//学習日欄
 let calendar = document.getElementById('calendar');
 
 btn_record_post.addEventListener("click", function () {
@@ -21,34 +21,63 @@ btn_record_post.addEventListener("click", function () {
 
 // バツボタンを押した時にモーダルを閉じる処理
 
-clear_btn.addEventListener("click",function(){
+btn_close.addEventListener("click", function () {
 
     modal.style.display = "none";
     modal_overlay.style.display = "none";
-    
 
-    
+
+
 });
 
 
 // カレンダー
 
-input_day_for_study.addEventListener("click",function(){
-    
+input_day_for_study.addEventListener("click", function () {
+
     modal.style.display = "none";
     calendar.style.display = "block";
 
     // let freamework = document.getElementById('freamework');
     // let new_div = document.createElement('div');
-    
+
     // freamework.appendChild(new_div);
     // document.getElementById('modal').innerHTML = new_div
-    
+
     // new_div.innerHTML = calendar
-    
+
     // only_freamework.style.display = "block"; 
 
 });
+
+
+
+let A = document.getElementById('A');
+let E = document.getElementById('E');
+let C = document.getElementById('C');
+let D = document.getElementsByClassName('fas fa-check-circle');
+const F = getComputedStyle(document.querySelector("#E"),"::before").content;
+
+// function ryuがあるため、他の✓の色がつかなくなった。ryuはspanタグについてるから・
+// function ryu() {
+   
+
+
+   
+ 
+
+// };
+
+
+
+$("#E, .fa-check-circle").on("click", function() {
+    $(this).toggleClass('fa-check-circle2');
+    A.classList.remove('O');
+    A.classList.toggle('AB');
+
+});
+
+
 
 
 
@@ -60,3 +89,47 @@ modal_record_post.addEventListener("click", function () {
 
 });
 
+
+
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------
+
+google.charts.load('current', { 'packages': ['corechart'] });
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+
+    var data = google.visualization.arrayToDataTable([
+        ['Effort', 'Amount given'],
+        ['JavaScript', 50.6],
+        ['HTML', 10],
+        ['SHEL', 10],
+        ['情報システム基礎知識 （その他）', 10],
+        ['CSS', 10],
+        ['Laravel', 10],
+        ['PHP', 10],
+        ['SQL', 10],
+
+
+
+    ]);
+
+    var options = {
+        pieHole: 0.5,
+        pieSliceTextStyle: {
+            color: 'black',
+        },
+        legend: 'none'
+    };
+
+    var chart = new google.visualization.PieChart(document.getElementById('donut_single'));
+    chart.draw(data, options);
+}
