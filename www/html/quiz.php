d27f85ce483e55ab8ddbaee5ffc4c3e54a82b0bc
加茂竜之介 <Kamoryu2001@gmail.com>
2022-03-04 23:15:05 +0900

    解説の追加

--- www/html/quiz.php
+++ www/html/quiz.php
@@ -44,19 +44,32 @@ foreach ($data2 as $val) {
     array_push($choices, $val['name']);
 };
 
+// print_r('<pre>');
+// print_r($choices);
+// print_r('</pre>');
+
 // シャッフル用の配列を作成
 $shuffle_array = [0,1,2];
 
-
+// 画像の数分ループさせることで、id=1のパターンとid=2のパターンを両立させることができる。
+// id=1の場合、画像は高輪と亀戸の二つのためループが２回行われ、問題数も２個だけつくられる。
+// 大切な前提として、そもそもid=1と2はそれぞれ違うページのことを指す。id=1とid=2は、全然違うページで、その内容を上のselect文でmysqlテーブルからデータをもってきている。
 for ($i = 1; $i < count($questions) + 1; $i++) { 
     // シャッフル関数を発火
     shuffle($shuffle_array);?>
 
     <section class="parent">
         <h2> <?php echo $i; ?> この地名はなんて読む？</h2>
+        <!-- 画像の処理は、questionsでもできる。$questions[0] or [1]、imageは書かなくていい-->
         <img src="./images/<?php echo $data1[$i - 1]['image']; ?>" alt="">
         <ul>
             <!-- if文で数字の調整をしている。 -->
+            <!-- 疑問に思うかもしれない。上のselect文でid=1もしくは2というように、どちらか一つしかないはず。それを利用したのが$questionsしかし、なんぜif文でid=1の時と２の時をかいているのか？ -->
+            <!-- 答えは、question_idの数が違う。id=1の時は、1か２。id=2の時は3。そうなると、chocies[0],[1][2]で選択肢を取り出したいのにできない。ちなみに、id=1とid=2でchocies[0]..の中身も変わる。 -->
+            <!-- これでjsを使うと1-0,1-1,1-2パターンがfor文でないと作れない-->
+            <!-- ちなみに、choicesは、配列、うえにある。Choicesとは違う -->
+            <!-- data3の配列を見れば分かるが、[0] questions_id = 1 [1] questions_id=2 -->
+            <!-- $chocies以降で最後足し算して[0].[1],[2]を作っている。それでchocies[0]...となる。 -->
             <?php if ($id == 1) { ?>
                 <li id="<?php echo $data3[$i - 1]['question_id'] - 1; ?>-0" class="button_style1"><?php echo $choices[($data3[$i - 1]['question_id'] - 1) * 3+$shuffle_array[0]]; ?></li>
                 <li id="<?php echo $data3[$i - 1]['question_id'] - 1; ?>-1" class="button_style2"><?php echo $choices[($data3[$i - 1]['question_id'] - 1) * 3 + $shuffle_array[1]]; ?></li>
